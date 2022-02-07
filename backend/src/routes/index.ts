30ce05049c24fafc064efa5ef3a5698a6cdf36e9
Thanatcha <tkhunkhe@gmail.com>
2022-02-07 21:21:15 +0700

    add index route

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,10 +1,19 @@
 import express from "express";
+import dashboard from "./dashboard";
+import admin from "./admin";
+import mobile from "./mobile";
+import bodyParser from "body-parser";
 
 const router = express.Router();
+const jsonParser = bodyParser.json({ limit: "5mb" });
+router.use(jsonParser);
 router.get("/", (_, res) =>
   res
     .status(200)
     .send("Welcome to Chainstarts Full-stack Test App ** Backend **")
 );
+router.use("dashboard", dashboard);
+router.use("admin", admin);
+router.use("mobile", mobile);
 
 export default router;
