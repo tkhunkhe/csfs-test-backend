9243b3191e8ac4bf5552d4f82221247deb51b266
Thanatcha <tkhunkhe@gmail.com>
2022-02-07 21:21:15 +0700

    add get all users latest homes test

--- backend/tests/services/home.test.ts
+++ backend/tests/services/home.test.ts
@@ -22,3 +22,11 @@ describe("test get user latest home", () => {
     expect(res.address).toBe(mockedUser.homes[1].address);
   });
 });
+
+describe("test get all users latest home", () => {
+  let userId = null;
+  let res = null;
+  it("should exist", async () => {
+    expect(res).toBeDefined();
+  });
+});
